715db082f3bddf731803e2f509bf4c66fac4da6b
kaelig02410@outlook.fr <kaelig02410@outlook.fr>
2022-01-22 17:03:11 +0100

    Add Research bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,20 @@
 <template>
+<div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  
+  <HelloWorld/>
+</div>
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import BarreRecherche from './components/BarreRecherche.vue'
 export default {
   name: 'App',
   components: {
+    BarreRecherche,
     HelloWorld
   }
 }
---
+++ client/src/components/BarreRecherche.vue
@@ -0,0 +1,13 @@
+<template>
+  
+    
+        <div form="relative"> 
+            <input type="text" class="h-14 w-96 pr-8 pl-5 rounded border z-0 focus:shadow focus:outline-none" placeholder="Boisson...">
+            <input type="text" class="h-14 w-96 pr-8 pl-5 rounded border z-0 focus:shadow focus:outline-none" placeholder="Budget..">
+            <input type="image" id="image"s
+                        src="./assets/Recherche.png">
+           
+        </div>
+   
+
+</template>
