45d252a698a82092c759d56de41eb5ea10621519
SwordOfNayru <theswordoflink@live.com>
2022-02-02 12:44:03 +0100

    Start of the back

---
+++ srv/routes/boissons/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const sequelize = require("../../sequelize/index");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    let queryArray = []
+    
+    if(req.query.name) {
+        
+    }
+
+});
+
+router.get("/:id", async (req, res, next) => {
+    const id = parseInt(req.params.id, 10);
+
+    if(isNaN(id)) {
+        next()
+        return;
+    } 
+    else {
+        const boissons = sequelize.model("boisson");
+        const boisson = await boissons.findOne({where : { id : req.params.id}});
+    
+        if(boisson != null) {
+            res.send(boisson)
+        }
+        else {
+            res.status(404).send(`Can't find ${req.params.id}`)
+        }
+    }
+
+})
+
+router.get("/test", async (req, res) => {
+    const typeBoisson = sequelize.model("type_boisson")
+    const created = await typeBoisson.create({
+        nom:"bonjour"
+    });
+
+    res.send(created);
+})
+
+module.exports = router
