360da60cccda1bc94e1abbebdf40a538d6c5a3c1
Pierre-Louis <pierre-louis@riposte-consulting.fr>
2022-02-06 14:20:35 +0100

    back

--- srv/routes/boissons/index.js
+++ srv/routes/boissons/index.js
@@ -3,42 +3,38 @@ const sequelize = require("../../sequelize/index");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    let queryArray = []
-    
-    if(req.query.name) {
-        
-    }
+    let queryArray = [];
 
+    if (req.query.name) {
+    }
 });
 
 router.get("/:id", async (req, res, next) => {
     const id = parseInt(req.params.id, 10);
 
-    if(isNaN(id)) {
-        next()
+    if (isNaN(id)) {
+        next();
         return;
-    } 
-    else {
+    } else {
         const boissons = sequelize.model("boisson");
-        const boisson = await boissons.findOne({where : { id : req.params.id}});
-    
-        if(boisson != null) {
-            res.send(boisson)
-        }
-        else {
-            res.status(404).send(`Can't find ${req.params.id}`)
+        const boisson = await boissons.findOne({
+            where: { id: req.params.id },
+        });
+
+        if (boisson != null) {
+            res.send(boisson);
+        } else {
+            res.status(404).send(`Can't find ${req.params.id}`);
         }
     }
+});
 
-})
+router.get("/type_boisson", async (req, res) => {
+    const typeBoisson = sequelize.model("type_boisson");
 
-router.get("/test", async (req, res) => {
-    const typeBoisson = sequelize.model("type_boisson")
-    const created = await typeBoisson.create({
-        nom:"bonjour"
-    });
+    const all = typeBoisson.findAll();
 
-    res.send(created);
-})
+    res.json(all);
+});
 
-module.exports = router
+module.exports = router;
